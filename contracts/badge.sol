888617e31f48ffc01b263f92c5a0ab8eb551fde7
xris003 <chrisikariagabriel@gmail.com>
2024-01-15 15:53:05 +0100

    contractupdate

--- contracts/badge.sol
+++ contracts/badge.sol
@@ -6,45 +6,6 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-// contract badge is ERC1155 {
-//     using Counters for Counters.Counter;
-//     Counters.Counter private _badgeId;
-
-//     address public merchant;
-//     address public user;
-//     uint256 platinum;
-//     uint256 diamond;
-//     uint256 gold;
-
-//     modifier onlyOwner() {
-//         require(
-//             msg.sender == merchant,
-//             "You're not authorised to perform this function"
-//         );
-//         _;
-//     }
-
-//      constructor() ERC1155("BadgeItem") {}
-
-//     function createbadge(string memory uri) public returns(uint256) {
-//         _badgeId.increment();
-//         merchant = msg.sender;
-//         uint256 badgeId = _badgeId.current();
-
-//         _mint(merchant, badgeId, 100, " ");
-
-//         _setURI(uri);
-
-//         return badgeId;
-//     }
-
-//     function badgeBal( uint badgeId) public view returns (uint256) {
-//         merchant = msg.sender;
-//         return balanceOf(badgeId, merchant);
-//     }
-
-// }
-
 contract Badge is ERC1155 {
     using Counters for Counters.Counter;
     Counters.Counter private _badgeId;
@@ -64,7 +25,6 @@ contract Badge is ERC1155 {
         address payable owner;
         address payable merchant;
         uint256 price;
-        bool currentlyListed;
     }
 
     // To map a badgeID to it's data.
@@ -79,7 +39,8 @@ contract Badge is ERC1155 {
 
     function createBadge(
         string memory uri,
-        uint256 price
+        uint256 price,
+        uint256 quantity
     ) public returns (uint256) {
         require(price > 0, "cannot set negative amount");
 
@@ -92,17 +53,18 @@ contract Badge is ERC1155 {
         uint256 badgeId = _badgeId.current();
         merchant = msg.sender;
 
-        _mint(merchant, badgeId, 100, " "); // You might want to provide a more meaningful URI here
+        _mint(merchant, badgeId, quantity, " "); // You might want to provide a more meaningful URI here
 
         _setURI(uri);
+
+        tokenURIExists[uri] = true;
 
         //Update the mapping of tokenId's to Token details, useful for retrieval functions
         idToListedBadge[badgeId] = ListedBadge(
             badgeId,
             payable(address(this)),
             payable(msg.sender),
-            price,
-            true
+            price
         );
 
         return badgeId;
@@ -114,10 +76,9 @@ contract Badge is ERC1155 {
         uint256 amount,
         uint256 badgeId
     ) public {
-        safeTransferFrom(merchant, buyer, amount, badgeId, "");
+        safeTransferFrom(_merchant, buyer, amount, badgeId, "");
 
-        isApprovedForAll(_merchant, buyer);
-        // isApprovedForAll(account, operator);
+        setApprovalForAll(address(this), true);
     }
 
     function badgeBal(uint256 badgeId) public view onlyOwner returns (uint256) {
@@ -136,8 +97,8 @@ contract Badge is ERC1155 {
 
         idToListedBadge[badgeId].merchant = payable(msg.sender);
 
-        contractBuy(_merchant, buyer, amount, badgeId);
-
         payable(_merchant).transfer(price);
+
+        contractBuy(_merchant, buyer, amount, badgeId);
     }
 }
